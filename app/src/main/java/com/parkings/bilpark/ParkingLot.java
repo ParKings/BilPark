422b96482808289f23d23d00c2ba3f59998a0943
Emre Acarturk <acarturk.emre1@gmail.com>
2018-05-12 22:59:12 +0300

    Fixed ParkingSpot#center & contains() algorithms. (v.3)
    Signed-off-by: Emre Acarturk <acarturk.emre1@gmail.com>

--- app/src/main/java/com/parkings/bilpark/ParkingLot.java
+++ app/src/main/java/com/parkings/bilpark/ParkingLot.java
@@ -7,10 +7,10 @@ import com.google.firebase.database.Exclude;
  * Created by uğur on 15.04.2018.
  * Last edited by Emre on 25.04.2018.
  * <p>
- * Last edited to add JavaDoc.
+ * Last edited to fix contains(LatLng).
  *
  * @author Ugur
- * @version 28.04.2018.0
+ * @version 12.05.2018.0
  */
 public class ParkingLot {
 	// Note: CORNERS ARE ASSIGNED IN COUNTERCLOCKWISE DIRECTION
@@ -65,26 +65,7 @@ public class ParkingLot {
 	 */
 	@Exclude
 	public boolean contains(LatLng latLng) {
-		double totalArea = 0.5 * Math.abs(
-				corners[0].latitude * corners[1].longitude +
-						corners[1].latitude * corners[2].longitude +
-						corners[2].latitude * corners[3].longitude +
-						corners[3].latitude * corners[0].longitude -
-						corners[1].latitude * corners[0].longitude -
-						corners[2].latitude * corners[1].longitude -
-						corners[3].latitude * corners[2].longitude -
-						corners[0].latitude * corners[3].longitude);
-		double resultingArea = 0;
-		for (int i = 0; i < 4; i++) {
-			resultingArea += 0.5 * Math.abs(
-					latLng.latitude * corners[0 + i].longitude +
-							corners[0 + i].latitude * corners[(1 + i) % 4].longitude +
-							corners[(1 + i) % 4].latitude * latLng.longitude -
-							corners[0 + i].latitude * latLng.longitude -
-							corners[(1 + i) % 4].latitude * corners[0 + i].longitude -
-							latLng.latitude * corners[(1 + i) % 4].longitude);
-		}
-		return resultingArea <= totalArea;
+		return PolygonUtil.isInside(corners, 4, latLng);
 	}
 
 	/**
