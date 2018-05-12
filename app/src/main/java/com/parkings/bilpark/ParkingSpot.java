b1170d8d6036a13bcfeb9af2f34e99b32ad06a75
Emre Acarturk <acarturk.emre1@gmail.com>
2018-05-12 22:31:54 +0300

    Fixed ParkingSpot#center & contains() algorithms.
    Signed-off-by: Emre Acarturk <acarturk.emre1@gmail.com>

--- app/src/main/java/com/parkings/bilpark/ParkingSpot.java
+++ app/src/main/java/com/parkings/bilpark/ParkingSpot.java
@@ -10,10 +10,16 @@ import java.util.HashMap;
 
 /**
  * Created by uğur on 15.04.2018.
- * Last edited by Emre Acarturk. Added JavaDoc.
+ * Last edited by Emre Acarturk. Fixed center finding and containment algorithms.
+ *
+ * Reused C++ code from:
+ * https://www.geeksforgeeks.org/how-to-check-if-a-given-point-lies-inside-a-polygon/
+ *
+ * Used an algorithm from:
+ * http://www.dcs.gla.ac.uk/~pat/52233/slides/Geometry1x1.pdf.
  *
  * @author Uğur
- * @version 28.04.2018.0
+ * @version 12.05.2018.0
  */
 public class ParkingSpot {
 	//CORNERS ARE ASSIGNED IN COUNTERCLOCKWISE DIRECTION
@@ -130,23 +136,105 @@ public class ParkingSpot {
 	 */
 	@Exclude
 	public boolean contains(LatLng latLng) {
-		double totalArea = 0.5 * Math.abs(corners[0].latitude * corners[1].longitude +
-				corners[1].latitude * corners[2].longitude +
-				corners[2].latitude * corners[3].longitude +
-				corners[3].latitude * corners[0].longitude -
-				corners[1].latitude * corners[0].longitude -
-				corners[2].latitude * corners[1].longitude -
-				corners[3].latitude * corners[2].longitude -
-				corners[0].latitude * corners[3].longitude);
-		double resultingArea = 0;
-		for (int i = 0; i < 4; i++) {
-			resultingArea += 0.5 * Math.abs(latLng.latitude * corners[0 + i].longitude +
-					corners[0 + i].latitude * corners[(1 + i) % 4].longitude +
-					corners[(1 + i) % 4].latitude * latLng.longitude -
-					corners[0 + i].latitude * latLng.longitude -
-					corners[(1 + i) % 4].latitude * corners[0 + i].longitude -
-					latLng.latitude * corners[(1 + i) % 4].longitude);
-		}
-		return resultingArea <= totalArea;
+		return isInside(corners, 4, latLng);
+	}
+
+	/*
+		The following static part of this class is converted from the C++ code from
+		https://www.geeksforgeeks.org/how-to-check-if-a-given-point-lies-inside-a-polygon/
+	 */
+	/*
+	 * Given three colinear LatLngs p, q, r, the function checks if
+	 * LatLng q lies on line segment 'pr'
+	 */
+	private static boolean onSegment(LatLng p, LatLng q, LatLng r)
+	{
+		if (q.latitude <= Math.max(p.latitude, r.latitude) && q.latitude >= Math.min(p.latitude, r.latitude) &&
+				q.longitude <= Math.max(p.longitude, r.longitude) && q.longitude >= Math.min(p.longitude, r.longitude))
+			return true;
+		return false;
+	}
+
+	/*
+		To find orientation of ordered triplet (p, q, r).
+		The function returns following values
+		0 --> p, q and r are colinear
+		1 --> Clockwise
+		2 --> Counterclockwise
+	*/
+	private static int orientation(LatLng p, LatLng q, LatLng r)
+	{
+		double val = (q.longitude - p.longitude) * (r.latitude - q.latitude) -
+				(q.latitude - p.latitude) * (r.longitude - q.longitude);
+
+		if (Math.abs(val) < EPSILON) return 0;  // colinear
+		return (val > 0)? 1: 2; // clock or counterclock wise
+	}
+
+	/*
+		The function that returns true if line segment 'p1q1'
+		and 'p2q2' intersect.
+	*/
+	private static boolean doIntersect(LatLng p1, LatLng q1, LatLng p2, LatLng q2)
+	{
+		// Find the four orientations needed for general and
+		// special cases
+		int o1 = orientation(p1, q1, p2);
+		int o2 = orientation(p1, q1, q2);
+		int o3 = orientation(p2, q2, p1);
+		int o4 = orientation(p2, q2, q1);
+
+		// General case
+		if (o1 != o2 && o3 != o4)
+			return true;
+
+		// Special Cases
+		// p1, q1 and p2 are colinear and p2 lies on segment p1q1
+		if (o1 == 0 && onSegment(p1, p2, q1)) return true;
+
+		// p1, q1 and p2 are colinear and q2 lies on segment p1q1
+		if (o2 == 0 && onSegment(p1, q2, q1)) return true;
+
+		// p2, q2 and p1 are colinear and p1 lies on segment p2q2
+		if (o3 == 0 && onSegment(p2, p1, q2)) return true;
+
+		// p2, q2 and q1 are colinear and q1 lies on segment p2q2
+		if (o4 == 0 && onSegment(p2, q1, q2)) return true;
+
+		return false; // Doesn't fall in any of the above cases
+	}
+
+	// Returns true if the LatLng p lies inside the polygon[] with n vertices
+	private static boolean isInside(LatLng polygon[], int n, LatLng p)
+	{
+		// There must be at least 3 vertices in polygon[]
+		if (n < 3)  return false;
+
+		// Create a LatLng for line segment from p to infinite
+		LatLng extreme = new LatLng(Double.MAX_VALUE, p.longitude);
+
+		// Count intersections of the above line with sides of polygon
+		int count = 0, i = 0;
+		do
+		{
+			int next = (i+1)%n;
+
+			// Check if the line segment from 'p' to 'extreme' intersects
+			// with the line segment from 'polygon[i]' to 'polygon[next]'
+			if (doIntersect(polygon[i], polygon[next], p, extreme))
+			{
+				// If the LatLng 'p' is colinear with line segment 'i-next',
+				// then check if it lies on segment. If it lies, return true,
+				// otherwise false
+				if (orientation(polygon[i], p, polygon[next]) == 0)
+					return onSegment(polygon[i], p, polygon[next]);
+
+				count++;
+			}
+			i = next;
+		} while (i != 0);
+
+		// Return true if count is odd, false otherwise
+		return count%2 == 1;
 	}
 }
